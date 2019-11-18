5195f597d71452b2692a46c1bb8a88f1a9494efb
Vikas Rajbhar <vikasrajbhar00@gmail.com>
2019-11-18 21:17:15 +0530

    Authorization Permissions

--- v1/routes/campgrounds.js
+++ v1/routes/campgrounds.js
@@ -60,13 +60,9 @@ router.get("/:id", function(req, res) {
 });
 
 // Edit Campground Route
-router.get("/:id/edit", function(req, res) {
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
     Campground.findById(req.params.id, function(err, foundCampground) {
-        if(err) {
-            res.redirect("/campgrounds");
-        } else {
             res.render("campgrounds/edit", {campground: foundCampground});
-        }
     });
 });
 
@@ -99,6 +95,25 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next) {
+    if(req.isAuthenticated()) {        
+        Campground.findById(req.params.id, function(err, foundCampground) {
+            if(err) {
+                res.redirect("/campgrounds");
+            } else {
+                // Does the user own the campground?
+                if(foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
