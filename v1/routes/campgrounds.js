877aa40b8c74e556aba861a4dc14af61da8f0398
Vikas Rajbhar <vikasrajbhar00@gmail.com>
2019-11-24 19:51:56 +0530

    Better Error Handling Implemented

--- v1/routes/campgrounds.js
+++ v1/routes/campgrounds.js
@@ -50,8 +50,9 @@ router.get("/new", middleware.isLoggedIn, function(req, res) {
 router.get("/:id", function(req, res) {
     //Find the campground with the provided ID
     Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
-        if(err) {
-            console.log(err);
+        if(err || !foundCampground) {
+            req.flash("error", "Campground not found");
+            res.redirect("back")
         } else {
             console.log(foundCampground);
             //Render show template with that campground
